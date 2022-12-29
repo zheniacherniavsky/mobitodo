9488cd97471fcdfbe27fc22904cb88875bd60939
Yauheni Charniauski <yauheni.charniauski.work@gmail.com>
2022-12-29 19:50:13 +0400

    rename app

--- ios/MobitodoTests/MobitodoTests.m
+++ ios/MobitodoTests/MobitodoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnTodoTests : XCTestCase
+@interface MobitodoTests : XCTestCase
 
 @end
 
-@implementation rnTodoTests
+@implementation MobitodoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
